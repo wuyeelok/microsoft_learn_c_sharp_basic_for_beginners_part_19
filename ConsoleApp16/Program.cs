c7a923a666a24c347bd8c3a8dd7eb6d43949fb5d
wuyeelok <wuyeelok@gmail.com>
2025-02-22 15:49:48 +0800

    Initial commit

---
+++ ConsoleApp16/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+var p = 16;
+Console.WriteLine($"Hello, World! Part {p}");
+
+// Specify the data source.
+List<int> scores = new List<int> { 180, 2, 97, 92, 81, 60, 300 };
+
+
+// Define the query expression.
+IEnumerable<string> scoreQuery =
+    from score in scores
+    where score > 80 && score % 2 == 0
+    orderby score descending
+    select $"{score} ";
+
+// Execute the query.
+foreach (var s in scoreQuery)
+{
+    Console.Write(s);
+}
+// Output: 300, 180, 92
+Console.WriteLine();
+Console.WriteLine($"Number of scores: {scoreQuery.Count()}");
+// Output: 3
