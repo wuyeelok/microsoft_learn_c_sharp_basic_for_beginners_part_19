5fb0c9c0efd1cb29246bc207117eb361c58ea111
wuyeelok <wuyeelok@gmail.com>
2025-02-22 17:16:05 +0800

    List of Pets per Person

--- ConsoleApp16/Program.cs
+++ ConsoleApp16/Program.cs
@@ -1,15 +1,25 @@
-﻿Console.WriteLine("Hello World! OOP");
+﻿using System.Diagnostics.Contracts;
+
+Console.WriteLine("Hello World! OOP");
 
 var p1 = new Person("John", "Doe", new DateOnly(1990, 1, 1));
+p1.Pets.Add(new Tiger("Tiger1"));
+p1.Pets.Add(new Tiger("Tiger2"));
 
 var p2 = new Person("Ken", "Wu", new DateOnly(1989, 1, 1));
+p2.Pets.Add(new Mouse("Mouse1"));
+p2.Pets.Add(new Tiger("Tiger3"));
 
 List<Person> people = [p1, p2];
 Console.WriteLine(people.Count());
 
 foreach (var person in people)
 {
-    Console.WriteLine($"{person.FirstName} {person.LastName} was born on {person.BirthDate}");
+    Console.WriteLine($"{person.FirstName} {person.LastName} was born on {person.BirthDate}, has {person.Pets.Count()} pets");
+    foreach (var pet in person.Pets)
+    {
+        Console.WriteLine($"{pet.Name} makes noise {pet.MakeNoise()}");
+    }
 }
 
 
@@ -20,22 +30,32 @@ public class Person(string firstName, string lastName, DateOnly birthDate)
     public DateOnly BirthDate { get; } = birthDate;
 
 
+    public List<Pet> Pets { get; } = [];
 }
 
-public class Mouse(string name)
+public abstract class Pet(string name)
 {
     public string Name { get; } = name;
 
-    public string Maze() => "I am in the maze";
-
+    public abstract string MakeNoise();
 }
 
-public class Tiger(string name)
+public class Mouse(string name) : Pet(name)
 {
-    public string Name { get; } = name;
 
-    public string Hunt()
+    public override string MakeNoise()
     {
-        return "I am hunting";
+        return "Maze Maze";
     }
+}
+
+
+
+public class Tiger(string name) : Pet(name)
+{
+    public override string MakeNoise()
+    {
+        return "Roar";
+    }
+
 }
